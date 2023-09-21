c3d8e97d3a766372dc0b0102491737910f52cfea
Fabrizzio <fabrizzio.vilchez.e@tutec.edu.pe>
2023-09-21 18:37:42 -0500

    commands to run on linux/mac

---
+++ execute_store.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=./backend/api_store
+export FLASK_DEBUG=true
+flask run --port=5002
---
+++ execute_users.sh
@@ -0,0 +1,3 @@
+export FLASK_APP=./backend/api_users
+export FLASK_DEBUG=true
+flask run --port=5001
